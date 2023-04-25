827378e2d7bda2d9a10418492b13737438a4f3b5
uchie333 <uchiematthew@gmail.com>
2023-04-25 03:57:15 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int i, print = 0, print_char = 0;
-	int flag, width, precision, size, buff_ind = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -32,12 +32,12 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flag = get_flag(format, &i);
+			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			print = handle_print(format, &i, list, buffer, flag, width, precision, size);
+			print = handle_print(format, &i, list, buffer, flags, width, precision, size);
 			if (print == -1)
 				return (-1);
 			print_char += print;
--- get_flags.c
+++ get_flags.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * get_flag - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flag:
+ * get_flags - Calculates active flags
+ * @format: Formatted string.
+ * @i: takes a parameter.
+ * Return: Flags:
  */
-int get_flag(const char *format, int *i)
+int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
 	int j, curr_i;
-	int flag = 0;
+	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
@@ -20,7 +20,7 @@ int get_flag(const char *format, int *i)
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
 			if (format[curr_i] == FLAGS_CH[j])
 			{
-				flag |= FLAGS_ARR[j];
+				flags |= FLAGS_ARR[j];
 				break;
 			}
 
@@ -30,5 +30,5 @@ int get_flag(const char *format, int *i)
 
 	*i = curr_i - 1;
 
-	return (flag);
+	return (flags);
 }
